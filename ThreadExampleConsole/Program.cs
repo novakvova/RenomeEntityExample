14e0ae6997e09e6b6cf8d27356dcf49774b99fa4
novakvova <novakvova@gmail.com>
2021-07-31 11:27:44 +0300

    Show files and async awit

---
+++ ThreadExampleConsole/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExampleConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            #region IO
+            // string currentDir = Directory.GetCurrentDirectory();
+            // Console.WriteLine(currentDir);
+            // string dir = "images";
+            // if(!Directory.Exists(dir))
+            // {
+            //     Directory.CreateDirectory(dir);
+            // }
+            // string path = Path.Combine(currentDir, dir);
+            //// string fileName = "alisa.txt";
+            // string fileName = "bob.txt";
+            // Console.WriteLine(path);
+            // string filePath = Path.Combine(path, fileName);
+
+
+
+            //using(FileStream fs = File.Create(filePath))
+            //{
+            //    string str = "Hello";
+            //    var bytes = Encoding.Unicode.GetBytes(str);
+            //    fs.Write(bytes);
+            //}
+
+            ////Pass the filepath and filename to the StreamWriter Constructor
+            //StreamWriter sw = new StreamWriter(filePath, true);
+            ////Write a line of text
+            //sw.WriteLine("Hello World!!");
+            ////Write a second line of text
+            //sw.WriteLine("From the StreamWriter class");
+            ////Close the file
+            //sw.Close();
+
+            //String line;
+            //StreamReader sr = new StreamReader(filePath);
+            //line = sr.ReadLine();
+            //while (line!=null)
+            //{
+            //    Console.WriteLine(line);
+            //    line = sr.ReadLine();
+            //}
+            //sr.Close();
+
+            #endregion
+
+            #region Thread
+            //Thread addUsersThread = new Thread(ThreadWriteFiles); 
+            //var threadId = Thread.CurrentThread.ManagedThreadId;
+            //Console.WriteLine("Головний потік "+ threadId);
+
+            //addUsersThread.Start();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Thread.Sleep(200);
+            //    Console.WriteLine("--------------");
+            //}
+            //Console.ReadKey();
+            //Console.WriteLine("Маin завершив роботу.");
+            #endregion
+
+            Console.WriteLine("Головний потік " + Thread.CurrentThread.ManagedThreadId);
+            Task<string> task = new Task<string>(ThreadWriteFiles);
+            task.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("--------------------");
+            }
+
+            var data = await task;// task.Wait();
+            Console.WriteLine(data);
+            
+        }
+
+        static string ThreadWriteFiles()
+        {
+            Console.WriteLine("Наш новий потік " + Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("++++++++++++++++");
+            }
+            return "ok";
+        }
+
+
+    }
+}
---
+++ WpfAppThread/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppThread
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(AddUsersThread);
+            thread.Start();
+            Debug.WriteLine("Main id "+Thread.CurrentThread.ManagedThreadId);
+        }
+        private void AddUsersThread()
+        {
+            Debug.WriteLine("Other id "+Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 30; i++)
+            {
+                Thread.Sleep(200);
+                Dispatcher.Invoke(() =>
+                {
+
+                    lblInfo.Content = i + 1;
+                });
+
+            }
+        }
+    }
+}
